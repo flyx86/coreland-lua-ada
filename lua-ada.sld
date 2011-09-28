2244a58c7a29af986611ac4abcccda4f3897f56a
Felix Krause <flyx@isobeef.org>
2011-09-28 18:08:53 +0200

    moved build system from sysdeps to gprbuild

--- lua-ada.sld
+++
@@ -1,6 +0,0 @@
-lua-check_raise.o
-lua-config.o
-lua-ext.o
-lua-lib.o
-lua-user_data.o
-lua.o
---
+++ src/lua-config.ads
@@ -0,0 +1,5 @@
+package Lua.Config is
+
+  Can_Propagate_Exceptions : constant Boolean := False;
+
+end Lua.Config;
